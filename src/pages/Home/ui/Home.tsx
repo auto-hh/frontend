2838fc3ae41c4162f9b98a17f9fa71da521afe17
Карпузов Богдан Константинович <bkkarpuzov@edu.hse.ru>
2026-03-23 17:55:23 +0300

    fix: added resume selecting into Home

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -2,6 +2,7 @@ import s from "./Home.module.css";
 import { Loader, ReloadPageButton } from "@/shared/ui";
 import { useVacancies, VacancyList } from "@/entities/vacancies";
 import { commonStyles } from "@/shared/styles";
+import { SelectResume } from "@/feature/selectResume";
 
 export function Home() {
     const { vacancies, isLoading, getVacancies } = useVacancies();
@@ -15,6 +16,8 @@ export function Home() {
             <h1 className={commonStyles.title}>Home</h1>
 
             {!!vacancies.length && !isLoading && <ReloadPageButton />}
+            {!!vacancies.length || isLoading || <SelectResume />}
+
             {isLoading ? <Loader /> : <VacancyList vacancies={vacancies} />}
 
             {!!vacancies.length || isLoading || (
