6d8d06756be18ffca916431c6272942f916c8b6d
Карпузов Богдан Константинович <bkkarpuzov@edu.hse.ru>
2026-03-28 12:22:47 +0300

    search can

--- src/features/SearchWithProfile/api/fetchSearchOpportunity.ts
+++ src/features/SearchWithProfile/api/fetchSearchOpportunity.ts
@@ -1,8 +1,6 @@
 import { api } from "@/shared/api";
 
 export async function fetchSearchOpportunity(): Promise<boolean> {
-    return true;
-
     try {
         await api.get("/user/has-profile");
         return true;
